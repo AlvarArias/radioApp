74c913e93a13650798806babf209e89efbb113f4
Alvar Arias <AlvarArias@users.noreply.github.com>
2023-01-10 13:23:14 +0100

    Update RadioObj.swift
    ok

--- RadioObj.swift
+++ RadioObj.swift
@@ -8,11 +8,7 @@
 import Foundation
 import UIKit
 
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let myChannellRAdio = try? newJSONDecoder().decode(MyChannellRAdio.self, from: jsonData)
-// file radios.json
+
 // informacion sacada de :
 // https://api.sr.se/api/v2/channels
 // https://api.sr.se/v2/channels?page=2 (proxima página)
